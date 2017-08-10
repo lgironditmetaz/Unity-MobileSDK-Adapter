b36a2e94d6177d9650e8475d8c76128a1f15cc95
Loïc GIRON DIT METAZ <loic.gironditmetaz@gmail.com>
2017-08-10 15:53:15 +0200

    Added an utils class to retrieve the Android activity and run code on the main thread

---
+++ Assets/Plugins/SmartAdServer/API/Utils/AndroidUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+using SmartAdServer.Unity.Library.Models;
+using SmartAdServer.Unity.Library.Constants;
+
+namespace SmartAdServer.Unity.Library.Utils
+{
+	class AndroidUtils
+	{
+		/// <summary>
+		/// The java object representing the activity displaying the game.
+		/// </summary>
+		private AndroidJavaObject _unityActivity;
+
+		private static readonly AndroidUtils _instance = new AndroidUtils();
+
+		private AndroidUtils() {
+			_unityActivity = new AndroidJavaClass (JavaClass.UnityPlayer).GetStatic<AndroidJavaObject> (JavaMethod.CurrentActivity);
+		}
+
+		public static AndroidUtils Instance
+		{
+			get
+			{
+				return _instance; 
+			}
+		}
+		
+		/// <summary>
+		/// Runs some code on Android UI thread.
+		/// </summary>
+		/// <param name="method">The method to run on UI thread.</param>
+		public void RunOnJavaUiThread (Action method)
+		{
+			GetUnityActivity ().Call (JavaMethod.RunOnUiThread, new AndroidJavaRunnable (method));
+		}
+
+		/// <summary>
+		/// Gets the activity displaying the game.
+		/// </summary>
+		/// <returns>The activity displaying the game.</returns>
+		public AndroidJavaObject GetUnityActivity ()
+		{
+			return _unityActivity;
+		}
+
+	}
+}
+
